6c5e8f9a6412332ecd078066b0aa5791d8f012a6
Stephen Tredger <stredger@gmail.com>
2013-05-27 23:08:06 -0700

    have stack of ints

---
+++ stack/stack.c
@@ -0,0 +1,49 @@
+
+#include "stack.h"
+#include <stdio.h>
+
+int push(int *ele, stack *s) {
+
+  if (s->head == &s->data[STACK_SIZE])
+    return -1;
+  *s->head++ = *ele;
+  //printf("%d\n", *ele);
+  //printf("%d\n", *(s->head-1));
+  //printf("%d", (int) s->head);
+  return 0;
+}
+
+
+int peek(stack *s) {
+  if (s->head == s->data)
+    return 0;
+  //printf("%d", (int) s->head);
+  return *(s->head-1);
+}
+
+
+int pop(stack *s) {
+  if (s->head == s->data)
+    return 0;
+  return *(--s->head);
+}
+
+
+stack *create_stack() {
+  stack *s = (stack*) malloc(sizeof(stack));
+  s->head = s->data;
+  return s;
+}
+
+
+int main(int argc, char *argv[]) {
+
+  stack *s = create_stack();
+  int i = 1;
+  int j = &i;
+
+  push(&i,s);
+  printf("%d", peek(s));
+
+  return 0;
+}
---
+++ stack/stack.h
@@ -0,0 +1,15 @@
+
+#include <stdlib.h>
+
+#define STACK_SIZE 65536
+
+typedef struct stack_t {
+  int data[STACK_SIZE];
+  int *head;
+} stack;
+
+stack *create_stack();
+int push(int *ele, stack *s);
+int peek(stack *s);
+int pop(stack *s);
+
