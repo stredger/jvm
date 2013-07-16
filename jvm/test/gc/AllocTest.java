33fc2b0c1e7c6096c2384786367741653ce9784b
Josh <josherickson10@gmail.com>
2013-07-16 14:47:29 -0700

    Now freeing in sweep()

--- jvm/test/gc/AllocTest.java
+++ jvm/test/gc/AllocTest.java
@@ -5,7 +5,8 @@ class AllocTest {
     public static void main(String[] args) {
 
 	//System.out.println("Hello World!");
-    String s = "josh";
+    String d = "Garbage";
+    d = "Not Garbage";
 	System.gc();
 
     }
